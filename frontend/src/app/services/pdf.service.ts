be74442b06b213abd55412deaada6442f1203d90
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2023-10-30 07:46:39 +0000

    Start pdf component

--- frontend/src/app/services/pdf.service.ts
+++ frontend/src/app/services/pdf.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import * as pdfjsLib from 'pdfjs-dist';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
