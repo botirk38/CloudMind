70e854e60ccc05911ea6a0055916ad1c0ed59513
Yassir_Maknaoui <zjac270@live.rhul.ac.uk>
2023-10-31 20:18:25 +0000

    Gradle config

---
+++ Backend/src/main/java/chatpdf/demo/OpenAiFetch.java
@@ -0,0 +1,12 @@
+package chatpdf.demo;
+
+import com.theokanning.openai.CompletionRequest;
+import com.theokanning.openai.OpenAiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+public class OpenAiFetch {
+    
+}
