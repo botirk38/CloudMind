8347783d6cc599f672c1e39b6df81f89579d1d7d
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2023-11-25 13:34:51 +0000

    change demo component to send a file to the backend

--- frontend/src/app/modules/demo/chat/chat.component.ts
+++ frontend/src/app/modules/demo/chat/chat.component.ts
@@ -3,47 +3,16 @@ import { PdfService } from 'src/app/services/pdf.service';
 import { HttpClient } from '@angular/common/http';
 import { switchMap, tap } from 'rxjs';
 
-interface Message {
-  text: string;
-  date: Date;
-  author: 'user' | 'AI';
-}
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit {
-  constructor(private pdfService: PdfService, private http: HttpClient) {}
+export class ChatComponent {
 
-  messages: Message[] | undefined;
-  samplePDF = '/assets/sample.pdf';
-
-  ngOnInit(): void {
-    this.messages = [];
-    this.http.get(this.samplePDF, { responseType: 'blob' }).subscribe(blob => {
-      const file = new File([blob], 'sample.pdf', { type: 'application/pdf' });
-      this.pdfService.sendPdfToBackend(file).subscribe(response => {
-        next: (response: any) => {
-          console.log(response);
-        };
-        err: (error: any) => {
-          console.log(error);
-        }
-      });
-    });
-  }
-  addMessage(text: string, author: 'user' | 'AI'): void {
-    const message = {
-      text,
-      date: new Date(),
-      author,
-    };
-    this.messages?.push(message);
-  }
-
-  }
+}
   
   
 
--- frontend/src/app/modules/demo/demo.component.ts
+++ frontend/src/app/modules/demo/demo.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PdfService } from 'src/app/services/pdf.service';
+
+interface Message {
+  text: string;
+  date: Date;
+  author: 'user' | 'AI';
+}
 
 @Component({
   selector: 'app-demo',
@@ -6,5 +14,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent {
+
+  constructor(private pdfService: PdfService, private http: HttpClient) {}
+
+  messages: Message[] | undefined;
+  samplePDF = '/assets/sample.pdf';
+
+  ngOnInit(): void {
+    this.messages = [];
+    this.http.get(this.samplePDF, { responseType: 'blob' }).subscribe(blob => {
+      const file = new File([blob], 'sample.pdf', { type: 'application/pdf' });
+      this.pdfService.sendPdfToBackend(file).subscribe(response => {
+        next: (response: any) => {
+          console.log(response);
+        };
+        err: (error: any) => {
+          console.log(error);
+        }
+      });
+    });
+  }
+  addMessage(text: string, author: 'user' | 'AI'): void {
+    const message = {
+      text,
+      date: new Date(),
+      author,
+    };
+    this.messages?.push(message);
+  }
 
 }
