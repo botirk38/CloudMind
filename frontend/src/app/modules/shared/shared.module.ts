a4c3110614f473f2301c177bbece0905c7738ecd
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2023-11-30 14:45:26 +0000

    move chat component to shared

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoComponent } from './video/video.component';
+import { ChatComponent } from './chat/chat.component';
 
 
 
 @NgModule({
   declarations: [
-    VideoComponent
+    VideoComponent,
+    ChatComponent
   ],
   imports: [
     CommonModule
