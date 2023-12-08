c997f6f6aaaf8338b64f20ff37c5dd3249a9f67b
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2023-12-08 12:09:35 +0000

    Fix line rendering in layout service

--- frontend/src/app/services/layout.service.ts
+++ frontend/src/app/services/layout.service.ts
@@ -8,10 +8,9 @@ export class LayoutService {
   averageMessageSize = { x: 400, y: 272 };
   textBoxPosition = { x: 0, y: 0 };
   textBoxSize = { width: 0, height: 0 };
-  lastMessagePosition: Coordinates = { x: 900, y: 625};
+  lastMessagePosition: Coordinates = { x: 900, y: 625 };
   isFirstMessage: boolean = true;
-  messageLines: SVGLineElement[] = []; // Array to track lines for each message
-
+  messageLines: SVGPathElement[] = []; // Array to track lines for each message
 
   // Stack to track message positions
   messageStack: Coordinates[] = [];
@@ -55,7 +54,7 @@ export class LayoutService {
       ? { ...this.lastMessagePosition }
       : {
           x: this.lastMessagePosition.x,
-          y: this.lastMessagePosition.y  - 30,
+          y: this.lastMessagePosition.y - 30,
         };
 
     // Add new message to the stack
@@ -64,7 +63,6 @@ export class LayoutService {
     // Draw line to the new message
     const newLine = this.drawLineToMessage(newPosition);
     this.messageLines.push(newLine);
-
 
     // Update lastMessagePosition for the next message, but only if it's not the first message
     if (!this.isFirstMessage) {
@@ -75,99 +73,115 @@ export class LayoutService {
 
     // Return the position for the new message
     return newPosition;
-}
+  }
 
-drawLineToMessage(newPosition: Coordinates): SVGLineElement {
-  const textBoxPosition = this.getTextBoxPosition();
-  const textBoxSize = this.getTextBoxSize();
+  drawLineToMessage(newPosition: Coordinates): SVGPathElement {
+    const textBoxPosition = this.getTextBoxPosition();
+    const textBoxSize = this.getTextBoxSize();
 
-  let startX: number;
-  let startY: number;
+    let startX: number;
+    let startY: number;
 
-  const textBoxMiddleY = textBoxPosition.y + textBoxSize.height / 2;
-  const messageMiddleY = newPosition.y + this.averageMessageSize.y / 2;
+    const textBoxMiddleY = textBoxPosition.y + textBoxSize.height / 2;
+    const messageMiddleY = newPosition.y + this.averageMessageSize.y / 2;
 
-  // Determine if the message is at the same vertical level as the textbox
-  const isAtSameLevel = Math.abs(textBoxMiddleY - messageMiddleY) <= this.averageMessageSize.y / 4;
+    // Determine if the message is at the same vertical level as the textbox
+    const isAtSameLevel =
+      Math.abs(textBoxMiddleY - messageMiddleY) <=
+      this.averageMessageSize.y / 4;
 
-  if (isAtSameLevel) {
+    if (isAtSameLevel) {
       // Start from the right middle of the textbox
       startX = textBoxPosition.x + textBoxSize.width;
       startY = textBoxPosition.y + textBoxSize.height / 2;
-  } else {
+    } else {
       // Start from either the top or bottom middle of the textbox
       startX = textBoxPosition.x + textBoxSize.width / 2;
-      startY = newPosition.y < textBoxPosition.y 
-               ? textBoxPosition.y 
-               : textBoxPosition.y + textBoxSize.height;
+      startY =
+        newPosition.y < textBoxPosition.y
+          ? textBoxPosition.y
+          : textBoxPosition.y + textBoxSize.height;
+    }
+
+    // End Y - Adjust to connect to the middle of the message box
+    const endX = newPosition.x;
+    const endY = newPosition.y + this.averageMessageSize.y / 2;
+
+    return this.createPath(startX, startY, endX, endY);
   }
 
-  // End Y - Adjust to connect to the middle of the message box
-  const endY = newPosition.y + this.averageMessageSize.y / 2;
+  createPath(
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number
+  ): SVGPathElement {
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const controlX = (startX + endX) / 2;
+    let controlY;
 
-  return this.createLine(startX, startY, newPosition.x, endY);
-}
+    if (endY > startY && Math.abs(endY - startY) > 30) {
+      // Message is below
+      controlY = Math.max(startY, endY) + 10; // Slight curve downward
+    } else {
+      // Message is above
+      controlY = Math.min(startY, endY) - 10; // Slight curve upward
+    }
 
+    const d = `M ${startX} ${startY} Q ${controlX} ${controlY}, ${endX} ${endY}`; // Simple line path; replace with a curve if needed
+    path.setAttribute('d', d);
+    path.setAttribute('stroke', 'black');
+    path.setAttribute('fill', 'none');
 
-
-
-  createLine(x1: number, y1: number, x2: number, y2: number): SVGLineElement {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', '100%');
     svg.setAttribute('height', '100%');
     svg.style.position = 'absolute';
     svg.style.top = '0';
     svg.style.left = '0';
-
-    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    line.setAttribute('x1', x1.toString());
-    line.setAttribute('y1', y1.toString());
-    line.setAttribute('x2', x2.toString());
-    line.setAttribute('y2', y2.toString());
-    line.setAttribute('stroke', 'black');
-
-    svg.appendChild(line);
-
+    svg.appendChild(path);
     document.body.appendChild(svg);
 
-    return line;
+    return path;
   }
 
-  updateLinePosition(line: SVGLineElement, messagePosition: Coordinates, index: number): void {
+  updateLinePosition(
+    path: SVGPathElement,
+    messagePosition: Coordinates,
+    index: number
+  ): void {
     const textBoxPosition = this.getTextBoxPosition();
     const textBoxSize = this.getTextBoxSize();
-  
+
     let startX: number;
     let startY: number;
-  
+
     const textBoxMiddleY = textBoxPosition.y + textBoxSize.height / 2;
-    const messageMiddleY = messagePosition.y + this.averageMessageSize.y / 2;
-  
-    // Determine if the message is at the same vertical level as the textbox
-    const isAtSameLevel = Math.abs(textBoxMiddleY - messageMiddleY) <= this.averageMessageSize.y / 4;
-  
-    if (isAtSameLevel) {
-        // Start from the right middle of the textbox
-        startX = textBoxPosition.x + textBoxSize.width;
-        startY = textBoxMiddleY;
-    } else {
-        // Start from either the top or bottom middle of the textbox
-        startX = textBoxPosition.x + textBoxSize.width / 2;
-        startY = messagePosition.y < textBoxPosition.y 
-                 ? textBoxPosition.y 
-                 : textBoxPosition.y + textBoxSize.height;
-    }
-  
-    // End Y - Adjust to connect to the middle of the message box
     const endY = messagePosition.y + this.averageMessageSize.y / 2;
-  
-    // Update line coordinates
-    line.setAttribute('x1', startX.toString());
-    line.setAttribute('y1', startY.toString());
-    line.setAttribute('x2', messagePosition.x.toString());
-    line.setAttribute('y2', endY.toString());
+
+    if (Math.abs(textBoxMiddleY - endY) <= this.averageMessageSize.y / 4) {
+      startX = textBoxPosition.x + textBoxSize.width;
+      startY = textBoxMiddleY;
+    } else {
+      startX = textBoxPosition.x + textBoxSize.width / 2;
+      startY =
+        endY < textBoxPosition.y
+          ? textBoxPosition.y
+          : textBoxPosition.y + textBoxSize.height;
+    }
+
+    const controlX = (startX + messagePosition.x) / 2;
+    let controlY;
+
+    if (endY > startY) {
+      controlY = Math.max(startY, endY) + 20;
+    } else {
+      controlY = Math.min(startY, endY) - 20;
+    }
+
+    const d = `M ${startX} ${startY} Q ${controlX} ${controlY}, ${messagePosition.x} ${endY}`;
+    path.setAttribute('d', d);
   }
-  
 
   // Method to reset message stack
   resetMessageStack(): void {
