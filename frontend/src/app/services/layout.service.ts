b73a0ca871d77de8871cdf4d41e54cf8a8839766
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2023-12-08 10:33:09 +0000

    Fix message positioning and draw lines to new messages

--- frontend/src/app/services/layout.service.ts
+++ frontend/src/app/services/layout.service.ts
@@ -9,6 +9,7 @@ export class LayoutService {
   textBoxPosition = { x: 0, y: 0 };
   textBoxSize = { width: 0, height: 0 };
   lastMessagePosition: Coordinates = { x: 900, y: 300 };
+  isFirstMessage: boolean = true;
 
   // Stack to track message positions
   messageStack: Coordinates[] = [];
@@ -39,28 +40,71 @@ export class LayoutService {
   }
 
   calculatePosition(): Coordinates {
-    // Push existing messages up
-    console.log("Before moving it up: ", this.messageStack)
-    console.log('averageMessageSize.y', this.averageMessageSize.y);
-    for (let i = 0; i < this.messageStack.length; i++) {
-      this.messageStack[i].y -= this.averageMessageSize.y + 20;
-    }
+    // Move existing messages up
+    this.messageStack.forEach((pos, index) => {
+      this.messageStack[index] = { x: pos.x, y: pos.y - this.averageMessageSize.y - 20 };
+    });
 
-    console.log("After moving it up: ", this.messageStack)
-
-    // Position for new message (at the bottom)
-    const newPosition = { 
-      x: this.lastMessagePosition.x, 
-      y: this.lastMessagePosition.y + this.averageMessageSize.y
-   };
-
-   this.lastMessagePosition = newPosition;
+    // Calculate position for the new message
+    const newPosition: Coordinates = this.isFirstMessage
+      ? { ...this.lastMessagePosition }
+      : {
+          x: this.lastMessagePosition.x,
+          y: this.lastMessagePosition.y + this.averageMessageSize.y + 20
+        };
 
     // Add new message to the stack
     this.messageStack.push(newPosition);
 
+    // Draw line to the new message
+    this.drawLineToMessage(newPosition);
+
+    // Update lastMessagePosition for the next message, but only if it's not the first message
+    if (!this.isFirstMessage) {
+      this.lastMessagePosition.y += this.averageMessageSize.y + 20;
+    } else {
+      this.isFirstMessage = false;
+    }
+
     // Return the position for the new message
     return newPosition;
+}
+
+drawLineToMessage(newPosition: Coordinates): void {
+  const textBoxPosition = this.getTextBoxPosition();
+  const textBoxSize = this.getTextBoxSize();
+
+  // Start Y - Bottom of the textbox or the top, based on the message position
+  const startY = newPosition.y < textBoxPosition.y 
+                 ? textBoxPosition.y 
+                 : textBoxPosition.y + textBoxSize.height;
+
+  const endY = newPosition.y + this.averageMessageSize.y / 2;
+
+  this.createLine(textBoxPosition.x + textBoxSize.width / 2, startY, newPosition.x, endY);
+}
+
+
+
+
+  createLine(x1: number, y1: number, x2: number, y2: number): void {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', '100%');
+    svg.setAttribute('height', '100%');
+    svg.style.position = 'absolute';
+    svg.style.top = '0';
+    svg.style.left = '0';
+
+    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x1', x1.toString());
+    line.setAttribute('y1', y1.toString());
+    line.setAttribute('x2', x2.toString());
+    line.setAttribute('y2', y2.toString());
+    line.setAttribute('stroke', 'black');
+
+    svg.appendChild(line);
+
+    document.body.appendChild(svg);
   }
 
   // Method to reset message stack
